58b17c2645a2ed61706ee9cc61e73396d8fbd5b7
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-12-02 07:35:31 +0200

    update head project + update dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps):JSX.Element {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>My top</title>
+      <link key={1} rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Component {...pageProps} />
+  </>
 }
