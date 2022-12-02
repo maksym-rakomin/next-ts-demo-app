332364bfabd286d4e9396eebfc5e3761f32dc5fb
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-12-02 11:46:52 +0200

    implement component Htag

---
+++ components/Htag/Htag.tsx
@@ -0,0 +1,7 @@
+import {HtagProps} from "./Htag.props";
+import { createElement } from 'react';
+import styles from './Htag.module.css';
+
+export const Htag = ({ tag, children }: HtagProps): JSX.Element => {
+    return createElement(tag, null, children )
+}
