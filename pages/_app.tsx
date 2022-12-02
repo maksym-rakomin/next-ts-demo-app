cc35b73550f222f6b829c467fe562a2fdc5553e3
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-12-02 09:31:26 +0200

    remove default content + add font + add variables to globals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps):JSX.Element {
-  return <>
-    <Head>
-      <title>My top</title>
-      <link key={1} rel="icon" href="/favicon.ico" />
-    </Head>
+export default function App({Component, pageProps}: AppProps): JSX.Element {
+    return <>
+        <Head>
+            <title>My top</title>
+            <link rel="icon" href="/favicon.ico"/>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@200;300;400;700&display=swap"
+                rel="stylesheet"
+            />
+        </Head>
 
-    <Component {...pageProps} />
-  </>
+        <Component {...pageProps} />
+    </>
 }
