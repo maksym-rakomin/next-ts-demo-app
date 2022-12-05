806ae1015d16cb2898ac7b2f90d38a8b443c9d8d
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-12-05 17:00:19 +0200

    add css grid to layout component

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,27 +1,22 @@
 import {LayoutProps} from "./Layout.props";
-import styles from './Ptag.module.css';
+import styles from './Layout.module.css';
 import cn from "classnames";
 import {Sidebar} from "./Sidebar/Sidebar";
 import {Header} from "./Header/Header";
 import {Footer} from "./Footer/Footer";
 import {FunctionComponent} from "react";
 
-const Layout = ({ children, ...props }: LayoutProps): JSX.Element => {
+const Layout = ({children, ...props}: LayoutProps): JSX.Element => {
 
     return (
-        <>
-            <Header />
-
-            <div>
-                <Sidebar />
-
-                <div>
-                    { children }
-                </div>
+        <div className={styles.wrapper}>
+            <Header className={styles.header}/>
+            <Sidebar className={styles.sidebar}/>
+            <div  className={styles.body}>
+                {children}
             </div>
-
-            <Footer />
-        </>
+            <Footer  className={styles.footer}/>
+        </div>
     )
 }
 
