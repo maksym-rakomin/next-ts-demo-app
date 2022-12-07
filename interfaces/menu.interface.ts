4dcd7882e7172a9693fe3cf4b39cc1375e659008
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-12-07 09:33:42 +0200

    implement layout component Menu

--- interfaces/menu.interface.ts
+++ interfaces/menu.interface.ts
@@ -1,8 +1,11 @@
+import {TopLevelCategory} from "./page.interface";
+
 export interface MenuItem  {
     _id: {
         secondCategory: string
     }
     pages: PageItem[]
+    isOpened?: boolean
 }
 
 export interface PageItem {
@@ -10,4 +13,11 @@ export interface PageItem {
     title: string
     _id: string
     category: string
+}
+
+export interface FirstLevelMenuItem {
+    route: string
+    name: string
+    icon: JSX.Element
+    id: TopLevelCategory
 }
