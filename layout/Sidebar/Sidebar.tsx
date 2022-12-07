a70c95ae47392a4bf24f817bfac73679b0fcb846
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-12-07 11:54:35 +0200

    add logo to sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,10 +2,16 @@ import { SidebarProps } from './Sidebar.props';
 import styles from './Sidebar.module.css';
 import cn from 'classnames';
 import {Menu} from "../Menu/Menu";
+import LogoItem from '../logo.svg'
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
 	return (
-		<div {...props}>
+		<div
+			{...props}
+			className={cn(className, styles.sidebar)}
+		>
+			<LogoItem className={styles.logo} />
+			<div>поиск</div>
 			<Menu />
 		</div>
 	);
