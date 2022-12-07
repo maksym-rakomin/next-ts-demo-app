a7b29976974cd1da8647e337dc2a42af1e1bc024
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-12-07 11:46:33 +0200

    implement helpers, page search and type + routing by menu

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -3,20 +3,9 @@ import cn from "classnames";
 import {useContext} from "react";
 import {AppContext} from "../../context/app.context";
 import {FirstLevelMenuItem, PageItem} from "../../interfaces/menu.interface";
-import CoursesIcon from './icons/courses.svg'
-import ServicesIcon from './icons/services.svg'
-import BooksIcon from './icons/books.svg'
-import ProductsIcon from './icons/products.svg'
-import {TopLevelCategory} from "../../interfaces/page.interface";
+import {firstLevelMenu} from '../../helpers/helpers'
 import Link from "next/link";
 import {useRouter} from "next/router";
-
-const firstLevelMenu: FirstLevelMenuItem[] = [
-	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
-]
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext)
@@ -29,6 +18,10 @@ export const Menu = (): JSX.Element => {
 			}
 			return m
 		}))
+	}
+
+	const goTo = (path: string) => {
+		path && router.push(path)
 	}
 
 	const buildFirstLevel = () => {
