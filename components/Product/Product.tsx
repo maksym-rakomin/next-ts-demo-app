ceec5ba6ac2c4f6d05ff5719b7e3edcf585027f0
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-12-08 17:31:21 +0200

    added a function for inflecting words with different numbers, completed the product component

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -5,7 +5,7 @@ import cn from "classnames";
  import {Rating} from "../Rating/Rating";
  import {Tag} from "../Tag/Tag";
  import {Button} from "../Button/Button";
- import {convertPrice} from "../../helpers/helpers";
+ import {convertPrice, declOfNum} from "../../helpers/helpers";
  import {Divider} from "../Divider/Divider";
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
@@ -32,12 +32,20 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
             </div>
             <div className={styles.priceTitle}>цена</div>
             <div className={styles.creditTitle}>кредит</div>
-            <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+            <div className={styles.rateTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
 
             <Divider className={styles.hr} />
 
             <div className={styles.description}>{product.description}</div>
-            <div className={styles.features}>features</div>
+            <div className={styles.features}>
+                {product.characteristics.map(c => (
+                    <div className={styles.characteristics} key={c.name}>
+                        <span className={styles.characteristicsName}>{c.name}</span>
+                        <span className={styles.characteristicsDots}></span>
+                        <span className={styles.characteristicsValue}>{c.value}</span>
+                    </div>
+                ))}
+            </div>
 
             <div className={styles.advBlock}>
                 {product.advantages && <div className={styles.advantages}>
