0407c409ad5c4866ef201de4126e5bcecd13b1fc
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-12-08 16:42:38 +0200

    implement component divider + add style to component Product

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -5,6 +5,8 @@ import cn from "classnames";
  import {Rating} from "../Rating/Rating";
  import {Tag} from "../Tag/Tag";
  import {Button} from "../Button/Button";
+ import {convertPrice} from "../../helpers/helpers";
+ import {Divider} from "../Divider/Divider";
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 
@@ -14,17 +16,25 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
                 <img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} />
             </div>
             <div className={styles.title}>{product.title}</div>
-            <div className={styles.price}>{product.price}</div>
-            <div className={styles.credit}>{product.credit}</div>
+            <div className={styles.price}>
+                {convertPrice(product.price)}
+                {product.oldPrice && <Tag className={styles.oldPrice} color="green">{convertPrice(product.price - product.oldPrice)}</Tag>}
+            </div>
+            <div className={styles.credit}>
+                {convertPrice(product.credit)}
+                /<span className={styles.month}>мес</span>
+            </div>
             <div className={styles.rate}><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
             <div className={styles.tags}>
-                { product.categories && product.categories.length > 0 && product.categories.map(c => <Tag key={c} color="ghost">{c}</Tag>)}
+                { product.categories
+                    && product.categories.length > 0
+                    && product.categories.map(c => <Tag className={styles.category} key={c} color="ghost">{c}</Tag>)}
             </div>
             <div className={styles.priceTitle}>цена</div>
             <div className={styles.creditTitle}>кредит</div>
             <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
 
-            <div className={styles.hr}><hr/></div>
+            <Divider className={styles.hr} />
 
             <div className={styles.description}>{product.description}</div>
             <div className={styles.features}>features</div>
@@ -40,7 +50,7 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
                 </div>}
             </div>
 
-            <div className={styles.hr}><hr/></div>
+            <Divider className={styles.hr} />
 
             <div className={styles.actions}>
                 <Button appearance='primary'>Узнать подробнее</Button>
