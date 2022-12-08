3a3210e7002a5fbc3ee9619f8e7df77f21336e0f
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-12-08 19:11:32 +0200

    add adaptive to section product

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -64,7 +64,7 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
                 </div>}
             </div>
 
-            <Divider className={styles.hr} />
+            <Divider className={cn(styles.hr, styles.hr2)} />
 
             <div className={styles.actions}>
                 <Button appearance='primary'>Узнать подробнее</Button>
