85fa9c67c67d9a43867eee32b9fc878937c5e13c
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-12-13 18:48:34 +0200

    fix working redux

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -3,7 +3,7 @@ import styles from './TopPageComponent.module.css';
 import {Advantages, HhData, Htag, Product, Sort, Tag} from "../../components";
 import {TopLevelCategory} from "../../interfaces/page.interface";
 import {SortEnum} from "../../components/Sort/Sort.props";
-import {useReducer} from "react";
+import {useEffect, useReducer} from "react";
 import {sortReducer} from "./sort.reducer";
 
 export const TopPageComponent = ({page, products, firstCategory}: TopPageComponentProps): JSX.Element => {
@@ -12,6 +12,10 @@ export const TopPageComponent = ({page, products, firstCategory}: TopPageCompone
     const setSort = (sort: SortEnum) => {
         dispatchSort({ type: sort })
     }
+
+    useEffect(() => {
+        dispatchSort({type: 'reset', initialState: products})
+    }, [products])
 
     return (
         <div className={styles.wrapper}>
