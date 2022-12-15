6956d3eefbe674abe724842509e9ad28f7907458
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-12-15 11:38:07 +0200

    hide CheckIcon.svg

--- components/Advantages/Advantages.tsx
+++ components/Advantages/Advantages.tsx
@@ -1,14 +1,14 @@
 import { AdvantagesProps } from './Advantages.props';
 import styles from './Advantages.module.css';
 import React from 'react';
-// import CheckIcon from './check.svg';
+import check from './check.svg';
 
 export const Advantages = ({ advantages }: AdvantagesProps): JSX.Element => {
 	return (
 		<>
 			{advantages.map(a => (
 				<div key={a._id} className={styles.advantage}>
-					{/*<CheckIcon />*/}
+					<check />
 					<div className={styles.title}>{a.title}</div>
 					<hr className={styles.vline} />
 					<div>{a.description}</div>
